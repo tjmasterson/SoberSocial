1441c7a68859ac7e6b0bc7cd207edfce190dfc26
tjmasterson <taylor.j.masterson@gmail.com>
2015-06-04 15:32:11 -0500

    associations working now"

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
 
-  belongs_to :creator, class_name: "User"
+  belongs_to :creator, class_name: "User", foreign_key: :user_id
   belongs_to :category
   has_many :event_guest_lists
   has_many :guests, through: :event_guest_lists, source: :user
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,14 @@ Category.create(title: "Tech")
 Category.create(title: "Food")
 Category.create(title: "Party")
 
-taylor.events.create(title: "Bowling", description: "Bowling tourney!!", address: "890 Saylor Ave Elmhurst IL", start_time: Faker::Time.between(DateTime.now - 1, DateTime.now), max_people: 10, num_peopel_attending: 7, category_id: 1)
+taylor.created_events.create(title: "Bowling", description: "Bowling tourney!!", address: "890 Saylor Ave Elmhurst IL", start_time: Faker::Time.between(DateTime.now - 1, DateTime.now), max_people: 10, num_peopel_attending: 7, category_id: 1)
 
-taylor.events.create(title: "Coffee", description: "Lets get coffee!!", address: "3463 N Clark St Chicago IL", start_time: Faker::Time.between(DateTime.now - 1, DateTime.now), max_people: 1, num_peopel_attending: 0, category_id: 7)
+taylor.created_events.create(title: "Coffee", description: "Lets get coffee!!", address: "3463 N Clark St Chicago IL", start_time: Faker::Time.between(DateTime.now - 1, DateTime.now), max_people: 1, num_peopel_attending: 0, category_id: 7)
 
-taylor.events.create(title: "Charity Event", description: "Blah is hosting a charity event", address: "953 W Belmont Chicago IL", start_time: Faker::Time.between(DateTime.now - 1, DateTime.now), max_people: 100, num_peopel_attending: 45, category_id: 8)
+taylor.created_events.create(title: "Charity Event", description: "Blah is hosting a charity event", address: "953 W Belmont Chicago IL", start_time: Faker::Time.between(DateTime.now - 1, DateTime.now), max_people: 100, num_peopel_attending: 45, category_id: 8)
 
-chase.events.create(title: "Pizza", description: "Pizza is just the best", address: "3033 N Ashland Ave Chicago IL", start_time: Faker::Time.between(DateTime.now - 1, DateTime.now), max_people: 2, num_peopel_attending: 1, category_id: 7)
+chase.created_events.create(title: "Pizza", description: "Pizza is just the best", address: "3033 N Ashland Ave Chicago IL", start_time: Faker::Time.between(DateTime.now - 1, DateTime.now), max_people: 2, num_peopel_attending: 1, category_id: 7)
 
-chase.events.create(title: "Dev Bootcamp", description: "Learn HTML for beginners", address: "2207 N Clybourn Ave Chicago IL", start_time: Faker::Time.between(DateTime.now - 1, DateTime.now), max_people: 8, num_peopel_attending: 5, category_id: 6)
+chase.created_events.create(title: "Dev Bootcamp", description: "Learn HTML for beginners", address: "2207 N Clybourn Ave Chicago IL", start_time: Faker::Time.between(DateTime.now - 1, DateTime.now), max_people: 8, num_peopel_attending: 5, category_id: 6)
 
-chase.events.create(title: "Bike Riding", description: "Let's go for a ride on lakeshore", address: "958 W Wrightwood Ave Chicago IL", start_time: Faker::Time.between(DateTime.now - 1, DateTime.now), max_people: 1, num_peopel_attending: 1, category_id: 3)
+chase.created_events.create(title: "Bike Riding", description: "Let's go for a ride on lakeshore", address: "958 W Wrightwood Ave Chicago IL", start_time: Faker::Time.between(DateTime.now - 1, DateTime.now), max_people: 1, num_peopel_attending: 1, category_id: 3)
