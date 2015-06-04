8d51bde73112726b037d78e299c8a00271e747e6
b0xw00d <chasenutile@gmail.com>
2015-06-04 13:05:48 -0500

    cleaned up code a bit

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,57 +1,31 @@
 $(document).ready(function(){
 
-  var mapCanvas = $("#map-canvas");
-  var address = mapCanvas.data("address");
-  var geocoder = new google.maps.Geocoder();
+  function initialize(){
+    var mapCanvas = $("#map-canvas");
+    var address = mapCanvas.data("address");
+    var geocoder = new google.maps.Geocoder();
 
-  geocoder.geocode({"address": address}, function(results, status){
-    var latitude = results[0].geometry.location.A;
-    var longitude = results[0].geometry.location.F;
-    initialize(latitude, longitude);
-  })//end geocode
+    geocoder.geocode({"address": address}, function(results, status){
+      var latitude = results[0].geometry.location.A;
+      var longitude = results[0].geometry.location.F;
+      buildMap(latitude, longitude);
+    })//end geocode
+  }//end init
 
-  function initialize(latitude, longitude){
-
+  function buildMap(latitude, longitude){
     var myLatlng = new google.maps.LatLng(latitude, longitude);
     var mapOptions = {
-      zoom: 8,
+      zoom: 10,
       center: myLatlng,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       mapTypeControl: false
     }
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
     var marker = new google.maps.InfoWindow({
       position: myLatlng,
       map: map,
-      content: "Yayayaya!"
+      content: "My childhood home!"
     });
-
-  }//end init
+  }//end buildMap
   google.maps.event.addDomListener(window, 'load', initialize);
 })//end $doc.ready
-
-
-
-
-// $(document).ready(function() {
-
-//   function initialize() {;
-//     var mapCanvas = $("#map-canvas");
-
-//     var myLatlng = new google.maps.LatLng(30,70);
-//     var mapOptions = {
-//       zoom: 4,
-//       center: myLatlng
-//     }
-
-//     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-//     var marker = new google.maps.InfoWindow({
-//         position: myLatlng,
-//         map: map,
-//         content: 'Yayayayaya!'
-//     });
-//   }
-//   google.maps.event.addDomListener(window, 'load', initialize);
-// })
