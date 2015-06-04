10a5f61882b4594f2fb15a89e32915e78b1106ed
b0xw00d <chasenutile@gmail.com>
2015-06-04 12:51:23 -0500

    maps are working with single pin

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,27 +1,57 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-  function initialize() {
+  var mapCanvas = $("#map-canvas");
+  var address = mapCanvas.data("address");
+  var geocoder = new google.maps.Geocoder();
 
-    var mapCanvas = $("#map-canvas");
-    var lat = mapCanvas.data("latitude");
-    var lng = mapCanvas.data("longitude");
+  geocoder.geocode({"address": address}, function(results, status){
+    var latitude = results[0].geometry.location.A;
+    var longitude = results[0].geometry.location.F;
+    initialize(latitude, longitude);
+  })//end geocode
 
-    var myLatlng = new google.maps.LatLng(lat,lng);
+  function initialize(latitude, longitude){
+
+    var myLatlng = new google.maps.LatLng(latitude, longitude);
     var mapOptions = {
-      zoom: 4,
-      center: myLatlng
+      zoom: 8,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeControl: false
     }
-
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title: 'Hello World!'
+    var marker = new google.maps.InfoWindow({
+      position: myLatlng,
+      map: map,
+      content: "Yayayaya!"
     });
-  }
 
-
+  }//end init
   google.maps.event.addDomListener(window, 'load', initialize);
+})//end $doc.ready
 
-})
+
+
+
+// $(document).ready(function() {
+
+//   function initialize() {;
+//     var mapCanvas = $("#map-canvas");
+
+//     var myLatlng = new google.maps.LatLng(30,70);
+//     var mapOptions = {
+//       zoom: 4,
+//       center: myLatlng
+//     }
+
+//     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+//     var marker = new google.maps.InfoWindow({
+//         position: myLatlng,
+//         map: map,
+//         content: 'Yayayayaya!'
+//     });
+//   }
+//   google.maps.event.addDomListener(window, 'load', initialize);
+// })
