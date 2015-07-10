568356f564a9b217193756d82886ee21c1e71e7f
b0xw00d <chasenutile@gmail.com>
2015-07-10 15:31:29 -0500

    stuff

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ActiveRecord::Base
   belongs_to :creator, class_name: "User", foreign_key: :user_id
   belongs_to :category
   has_many :event_guest_lists
-  has_many :guests, through: :event_guest_lists, source: :user
+  has_many :guests, through: :event_guest_lists, source: :user_id
 
   include PgSearch
   pg_search_scope :search,
